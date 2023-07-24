78a534c8b6258ba613f4749e4f34a81dea3a1bbb
mosehun <shmo@akuo.ai>
2023-07-24 13:57:43 +0900

    feature: 로그인 페이지 레이아웃 적용

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,37 +1,73 @@
 <script setup lang="ts">
+import {Icon} from '@iconify/vue';
+
 definePageMeta({
   layout: "blank",
 });
+
+const redirectGoogleLogin = () => {
+  window.location.href = "https://naver.com"
+}
 </script>
 
 <template>
-<VContainer>
-  <VCard>
-    <VCardItem title="Login"/>
-    <VCardItem class="justify-center">
-      <VLabel>Email</VLabel>
-      <VInput label="Email"></VInput>
+  <VApp id="inspire" style="position:relative">
+    <VContainer
+        style="margin: auto"
+    >
+      <VRow
+          justify="center"
+      >
+        <VCol cols="12" sm="4">
+          <VCard>
+            <VCardTitle class="text-center">Login</VCardTitle>
 
-      <VLabel>Password</VLabel>
-      <VInput></VInput>
+            <VCardItem>
+              <VLabel>Email</VLabel>
+<!--              todo VTextField 색깔 통일하기-->
+              <VTextField base-color="" bg-color="#FFFFFF" type="text"></VTextField>
 
-      <VCheckbox>Remember me?</VCheckbox>
+              <VLabel>Password</VLabel>
+              <VTextField base-color="" bg-color="#FFFFFF" type="password"></VTextField>
 
-      <VBtn>Login</VBtn>
-    </VCardItem>
+<!--              todo 네모박스 체크박스 적용하기-->
+              <VCheckbox label="Remember me?"></VCheckbox>
+<!--todo 색깔 통일하기-->
+              <VBtn width="100%">Login</VBtn>
+            </VCardItem>
+            <div class="hr-sect">or</div>
+            <VCardItem>
 
-    <VCardItem>
-      <hr/>
-      <VIcon></VIcon>
-    </VCardItem>
-    <VCardItem>
+              <VImg @click="redirectGoogleLogin" src="google_login.png"/>
+            </VCardItem>
+            <VCardItem>
 
-    </VCardItem>
+            </VCardItem>
 
-  </VCard>
-</VContainer>
+          </VCard>
+        </VCol>
+      </VRow>
+    </VContainer>
+  </VApp>
 </template>
 
 <style scoped>
-
+.hr-sect {
+  display: flex;
+  flex-basis: 100%;
+  align-items: center;
+  color: rgba(0, 0, 0, 0.35);
+  font-size: 12px;
+  margin: 8px 0px;
+}
+.hr-sect::before,
+.hr-sect::after {
+  content: "";
+  flex-grow: 1;
+  background: rgba(0, 0, 0, 0.35);
+  height: 1px;
+  font-size: 0px;
+  line-height: 0px;
+  margin: 0px 16px;
+}
 </style>
