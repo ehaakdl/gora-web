6ed1d013376947094ac70f1bba6b460a9d334d89
mosehun <shmo@akuo.ai>
2023-08-30 11:40:28 +0900

    feature:eslint 설정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,29 @@
-require("@rushstack/eslint-patch/modern-module-resolution")
-
+// eslint docs
+// https://eslint.org/docs/latest/use/configure/parser
 
 module.exports = {
-  extends: [
-    'eslint:recommended',
-    'plugin:vue/vue3-essential',
-    '@vue/eslint-config-typescript'
+  "env": {
+    // 사전 정의된 전역변수 docs
+    // https://eslint.org/docs/latest/use/configure/language-options#specifying-environments
+    // 명시해줘야 console, debugger 이런거 쓸 수 있음
+    "browser": true,
+    "node": true,
+    "es6":true
+  },
+  "extends": [
+    "airbnb-base",
+    "plugin:vue/recommended"
   ],
+  "parserOptions": {
+        // js 버전지정
+        "ecmaVersion": "6",
+        // typescript parser docs
+        // https://typescript-eslint.io/packages/parser/
+        "parser": "@typescript-eslint/parser",
+        // es6에서 사용될 때 사용
+        "sourceType": "module"
+  },
+  ignorePatterns:[],
   rules: {
     "vue/script-indent": ["error", 2, { "baseIndent": 0 }],
     "no-console": process.env.NODE_ENV === 'production' ? 'error' : "warn",
