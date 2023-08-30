33eb6ba10d45f78295813de28d9f1045c69fa6b2
mosehun <shmo@akuo.ai>
2023-08-30 11:48:11 +0900

    refactor: eslint 설정 fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,32 +2,32 @@
 // https://eslint.org/docs/latest/use/configure/parser
 
 module.exports = {
-  "env": {
+  env: {
     // 사전 정의된 전역변수 docs
     // https://eslint.org/docs/latest/use/configure/language-options#specifying-environments
     // 명시해줘야 console, debugger 이런거 쓸 수 있음
-    "browser": true,
-    "node": true,
-    "es6":true
+    browser: true,
+    node: true,
+    es6: true,
   },
-  "extends": [
-    "airbnb-base",
-    "plugin:vue/recommended"
+  extends: [
+    'airbnb-base',
+    'plugin:vue/recommended',
   ],
-  "parserOptions": {
-        // js 버전지정
-        "ecmaVersion": "6",
-        // typescript parser docs
-        // https://typescript-eslint.io/packages/parser/
-        "parser": "@typescript-eslint/parser",
-        // es6에서 사용될 때 사용
-        "sourceType": "module"
+  parserOptions: {
+    // js 버전지정
+    ecmaVersion: '6',
+    // typescript parser docs
+    // https://typescript-eslint.io/packages/parser/
+    parser: '@typescript-eslint/parser',
+    // es6에서 사용될 때 사용
+    sourceType: 'module',
   },
-  ignorePatterns:[],
+  ignorePatterns: [],
   rules: {
-    "vue/script-indent": ["error", 2, { "baseIndent": 0 }],
-    "no-console": process.env.NODE_ENV === 'production' ? 'error' : "warn",
-    "no-debugger": process.env.NODE_ENV === 'production' ? 'error' : "warn",
-    'vue/multi-word-component-names': 'off'
-  }
-}
+    'vue/script-indent': ['error', 2, { baseIndent: 0 }],
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'vue/multi-word-component-names': 'off',
+  },
+};
