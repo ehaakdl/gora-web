81efb63a01d03e3dbaaeaccf1f24413824563761
mosehun <ehaakdl@naver.com>
2023-11-23 18:48:15 +0900

    feature: temp commit

--- src/types.d/user.ts
+++ src/types.d/user.ts
@@ -2,7 +2,10 @@ export interface LoginRequest{
     email:string,
     password:string
 }
-
+export const defautlLoginRequest:LoginRequest = {
+  email: '',
+  password: '',
+};
 export interface SignupRequest{
     email:string,
     password:string
